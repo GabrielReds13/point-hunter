382dfc76dd82a249a8886286979501f31f581116
gabrielreds13 <speedckprod4007@gmail.com>
2024-11-04 16:45:24 -0400

    Feat: Added timer

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const menu = {
     currentScore: document.getElementById("currentScore"),
     bestScore: document.getElementById("bestScore"),
+    countdown: document.getElementById("countdown")
 }
 
 // ball
@@ -22,24 +23,74 @@ const ball = {
     
 }
 
-function ballClick() {
+async function ballClick() {
     // toogle
     ball.tooglePosition();
+    game.sumScore();
+    game.roundTimer.setCountdown();
+    await window.clearTimeout();
+    game.roundTimer.cronometer();
 
-    
 }
 
-
-
+// player
 const player = {
     bestScore: 0,
+    currentScore: 0
 }
 
+// game settings
 const game = {
-    roundDelay: {
-        min: 2,
-        max: 6,
-    },
+    // score
     scorePoint: 100,
+    sumScore() {
+        player.currentScore += this.scorePoint;
+        menu.currentScore.innerHTML = player.currentScore;
+        
+        // best score
+        if(player.currentScore >= player.bestScore) {
+            player.bestScore = player.currentScore;
+            menu.bestScore.innerHTML = player.bestScore;
+        }
+    },
+    clearScore() {
+        player.currentScore = 0;
+    },
+    // timer
+    roundTimer: {
+        counterTime: 0,
+        countdown: 0,
+        // dificulty
+        dificulty: {
+            v1: 6,
+            v2: 5,
+            v3: 4,
+            v4: 3,
+            v5: 2,
+        },
+        dScore: {
+            stage1: 750,
+            stage2: 2000,
+            stage3: 4000,
+            stage4: 8000,
+        },
+        cronometer() {
+            setTimeout(() => {
+                this.countdown -= 1;
+                menu.countdown.innerHTML = this.countdown;
+                this.cronometer();
+            }, 1000);
+        },
+        clearCronometer() {
+            clearTimeout();
+        },
+        setCountdown() {
+            if(player.currentScore >= this.dScore.stage1 && player.currentScore < this.dScore.stage2) this.countdown = this.dificulty.v2;
+            else if(player.currentScore >= this.dScore.stage2 && player.currentScore < this.dScore.stage3) this.countdown = this.dificulty.v3;
+            else if(player.currentScore >= this.dScore.stage3 && player.currentScore < this.dScore.stage4) this.countdown = this.dificulty.v4;
+            else if(player.currentScore >= this.dScore.stage4) this.countdown = this.dificulty.v5;
+            else  this.countdown = this.dificulty.v1;
+        }
+    },
 }
 
