66be390b88b9d6d8fce089d48b25529e30e9a7d4
GabrielReds13 <speedckprod4007@gmail.com>
2024-11-05 02:52:29 -0300

    Feat: Finish Idea

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+// audios
+const audios = {
+    startGame: new Audio("./assets/sounds/start-game-sound.mp3"),
+    tryAgain: new Audio("./assets/sounds/try-again-sound.mp3"),
+    lose: new Audio("./assets/sounds/lose-sound.mp3"),
+    newRecord: new Audio("./assets/sounds/new-record-sound.mp3"),
+    countdown: new Audio("./assets/sounds/countdown-sound.mp3"),
+    ballClick: new Audio("./assets/sounds/ball-click.mp3"),
+}
+
+// code
 const menu = {
     currentScore: document.getElementById("currentScore"),
     bestScore: document.getElementById("bestScore"),
-    countdown: document.getElementById("countdown")
+    timerSection: document.getElementById("timerSection"),
+    countdown: document.getElementById("countdown"),
+    playButton: document.getElementById("playButton"),
+    messageContainer: document.getElementById("messageContainer"),
+    messageText: document.getElementById("message"),
 }
 
 // ball
@@ -23,16 +38,6 @@ const ball = {
     
 }
 
-async function ballClick() {
-    // toogle
-    ball.tooglePosition();
-    game.sumScore();
-    game.roundTimer.setCountdown();
-    await window.clearTimeout();
-    game.roundTimer.cronometer();
-
-}
-
 // player
 const player = {
     bestScore: 0,
@@ -42,55 +47,131 @@ const player = {
 // game settings
 const game = {
     // score
-    scorePoint: 100,
-    sumScore() {
-        player.currentScore += this.scorePoint;
-        menu.currentScore.innerHTML = player.currentScore;
-        
-        // best score
-        if(player.currentScore >= player.bestScore) {
-            player.bestScore = player.currentScore;
-            menu.bestScore.innerHTML = player.bestScore;
-        }
+    score: {
+        point: 100,
+        sumScore() {
+            player.currentScore += this.point;
+            menu.currentScore.innerHTML = player.currentScore;
+            
+            // best score
+            if(player.currentScore >= player.bestScore) {
+                player.bestScore = player.currentScore;
+                menu.bestScore.innerHTML = player.bestScore;
+            }
+        },
+        clearScore() {
+            player.currentScore = 0;
+            menu.currentScore.innerHTML = player.currentScore;
+        },
     },
-    clearScore() {
-        player.currentScore = 0;
-    },
+
     // timer
     roundTimer: {
-        counterTime: 0,
-        countdown: 0,
         // dificulty
         dificulty: {
-            v1: 6,
-            v2: 5,
-            v3: 4,
-            v4: 3,
-            v5: 2,
+            v1: 5,
+            v2: 4,
+            v3: 3,
+            v4: 2,
+            v5: 1,
         },
         dScore: {
             stage1: 750,
             stage2: 2000,
-            stage3: 4000,
-            stage4: 8000,
-        },
-        cronometer() {
-            setTimeout(() => {
-                this.countdown -= 1;
-                menu.countdown.innerHTML = this.countdown;
-                this.cronometer();
-            }, 1000);
-        },
-        clearCronometer() {
-            clearTimeout();
+            stage3: 6000,
+            stage4: 10000,
         },
         setCountdown() {
-            if(player.currentScore >= this.dScore.stage1 && player.currentScore < this.dScore.stage2) this.countdown = this.dificulty.v2;
-            else if(player.currentScore >= this.dScore.stage2 && player.currentScore < this.dScore.stage3) this.countdown = this.dificulty.v3;
-            else if(player.currentScore >= this.dScore.stage3 && player.currentScore < this.dScore.stage4) this.countdown = this.dificulty.v4;
-            else if(player.currentScore >= this.dScore.stage4) this.countdown = this.dificulty.v5;
-            else  this.countdown = this.dificulty.v1;
+            if(player.currentScore >= this.dScore.stage1 && player.currentScore < this.dScore.stage2) return this.dificulty.v2;
+            else if(player.currentScore >= this.dScore.stage2 && player.currentScore < this.dScore.stage3) return this.dificulty.v3;
+            else if(player.currentScore >= this.dScore.stage3 && player.currentScore < this.dScore.stage4) return this.dificulty.v4;
+            else if(player.currentScore >= this.dScore.stage4) return this.dificulty.v5;
+            else  return this.dificulty.v1;
         }
+    },
+    messages() {
+        function phrases() {
+            if(player.currentScore < player.bestScore || player.currentScore === 0) {
+                audios.lose.play();
+                return "GAME OVER";
+            }
+            else {
+                audios.newRecord.play();
+                return "NEW SCORE";
+            }
+        }
+
+        menu.messageText.innerHTML = phrases();
+    }
+}
+
+let cronometer = {
+    timerId: null,
+    countdown: game.roundTimer.setCountdown(),
+    // start countdown
+    start() {
+        if (!this.timerId) {
+            this.timerId = setInterval(() => {
+                if(this.countdown > 0 && this.countdown <= 3) {
+                    audios.countdown.currentTime = 0;
+                    audios.countdown.play();
+                } 
+                if (this.countdown < 1) {
+                    menu.messageContainer.style.display = "flex";
+                    ball.ball.style.display = "none";
+                    game.messages();
+                    this.reset();
+                } else {
+                    this.countdown -= 1;
+                    this.updateDisplay();
+                }
+            }, 1000);
+        }
+    },
+    
+    // reset countdown
+    reset() {
+        clearInterval(this.timerId);
+        this.timerId = null;
+        this.countdown = game.roundTimer.setCountdown();
+        this.updateDisplay();
+    },
+    
+    // update timer dom
+    updateDisplay() {
+        menu.countdown.innerHTML = this.countdown;
     },
 }
 
+// other functions
+async function ballClick() {
+    // toogle
+    ball.tooglePosition();
+    game.score.sumScore();
+    cronometer.reset();
+    cronometer.start();
+    audios.ballClick.currentTime = 0;
+    audios.ballClick.play();
+}
+
+async function startGame(type) {
+    cronometer.reset();
+
+    if(type === "play") {
+        menu.playButton.style.display = "none";
+        menu.timerSection.style.display = "flex";
+        audios.startGame.play();
+    }
+    else if(type === "tryAgain") {
+        menu.messageContainer.style.display = "none";
+        game.score.clearScore();
+        audios.tryAgain.play();
+    }
+
+    ball.ball.style.display = "flex";
+    
+    ball.ball.style.left = `46%`;
+    ball.ball.style.top = `46%`;
+
+    cronometer.start();
+}
